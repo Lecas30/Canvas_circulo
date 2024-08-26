32ba1573bd22463732bf71b6c0153065a4306ac7
Lecas30 <leca30santos@gmail.com>
2024-08-26 15:15:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,27 +2,31 @@ canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 color = "red";
 ctx.beginPath();
-ctx.strokestyle = color;
-ctx.strokestyle.linewidth = 2;
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
 ctx.arc(200, 200, 40, 0, 2* Math.PI);
 ctx.stroke();
+
+canvas.addEventListener("mousedown", myMousedown);
+
 function myMousedown(e)
 {
-    color = document.getElemmentById("color").value;
+    color = document.getElementById("color").value;
     console.log;(color);
 
     mouseX = e.clientX - canvas.offsetLeft;
     mouseY = e.clientY - canvas.offsetTop;
 
-    console.log("X = " + mouseX+",Y= " + mouseY)
-    circle(mouseX , mouseY)
+    console.log("X = " + mouseX+",Y= " + mouseY);
+    circle(mouseX , mouseY);
 }
 function circle(mouseX , mouseY)
 {
     ctx.beginPath();
-    ctx.strokestyle = color;
+    ctx.strokeStyle = color;
     ctx.lineWidth = 2;
     ctx.arc(mouseX, mouseY, 40 ,0 ,2*Math.PI);
+    ctx.stroke();
 }
 
 function clearArea()
